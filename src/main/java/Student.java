867f02360a496f59cbafc1ac76addeaa35a2c01e
Lex Zavala <lex.zavala7@gmail.com>
2021-06-15 16:14:12 -0500

    started working on exercise

---
+++ src/main/java/Student.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Student {
+
+    private static String name;
+    private static long id;
+    private static ArrayList<Integer> grades;
+
+
+    public Student() {
+
+    }
+}
---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StudentTest {
+    private Student testStude = new Student();
+
+    @Before
+    public static void setUp(){
+
+
+    }
+
+    @Test
+    public void propertiesAreSet(){
+//        assertNotNull("Lex", );
+        assertEquals("Lex",testStudent);
+
+    }
+
+}
