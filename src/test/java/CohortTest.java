9bd91c27c81b4961428aa322d106ac9f913a148b
Lex Zavala <lex.zavala7@gmail.com>
2021-06-16 10:45:09 -0500

    Completed exercise

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -8,9 +8,39 @@ public class CohortTest {
 
     @Test
     public void testIfAddStudentWorks() {
+        Cohort testCohort = new Cohort();
         assertEquals(0, testCohort.getStudents().size());
-        testCohort.addStudent(student1);
+        testCohort.addStudent(new Student(7, "Tim"));
         assertEquals(1, testCohort.getStudents().size());
+        Student stud = new Student(3,"Stud Body");
+        testCohort.addStudent(stud);
+        assertEquals(stud, testCohort.getStudents().get(1));
+    }
+
+    @Test
+    public void testGetStudents() {
+        Cohort cohort = new Cohort();
+        cohort.addStudent(new Student(8, "Jim Johnly"));
+        cohort.addStudent(new Student(8, "Jim Johnly"));
+        cohort.addStudent(new Student(8, "Jim Johnly"));
+        assertEquals(3, cohort.getStudents().size());
+        assertEquals("Jim Johnly", cohort.getStudents().get(0).getName());
+    }
+
+    @Test
+    public void testGetCohortAverage(){
+        Cohort cohort = new Cohort();
+        Student stud = new Student(1, "Stud Body");
+        stud.addGrade(50);
+        stud.addGrade(25);
+        stud.addGrade(75);
+        Student jenn = new Student(12, "Jenn Ann Hologram");
+        jenn.addGrade(100);
+        jenn.addGrade(100);
+        jenn.addGrade(100);
+        cohort.addStudent(stud);
+        cohort.addStudent(jenn);
+        assertEquals(75, cohort.getCohortAverage(), 0);
     }
 
 }
